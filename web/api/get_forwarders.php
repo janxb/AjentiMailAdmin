1b222e99c1c4900eb37428ab7784e712378be5a8
janxb <github@janbrodda.de>
2016-03-20 12:45:11 +0100

    added first api methods (add_forwarder/get_forwarder)

---
+++ web/api/get_forwarders.php
@@ -0,0 +1,24 @@
+<?php
+    require('../index.php');
+    $owner_email = $_GET['email'];
+
+    $targets = array();
+    $response = new Response();
+
+    foreach ($mailconfig->forwarding_mailboxes as $mailbox){
+        $mailbox_email = $mailbox->local . '@' . $mailbox->domain;
+        if ($mailbox_email == $owner_email){
+            foreach ($mailbox->targets as $target) {
+                array_push($targets, $target->email);
+            }
+        }
+    }
+
+    if (empty($targets)){
+        $response->error = "address_not_found";
+    } else {
+        $response->data = $targets;
+    }
+
+    $response->send();
+?>
