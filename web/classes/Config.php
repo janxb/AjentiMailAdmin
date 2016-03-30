d7697084b0509efd20caec53b79f6f4aa6d87b74
janxb <github@janbrodda.de>
2016-03-30 15:58:38 +0200

    removed obsolete config parameter

--- web/classes/Config.php
+++ web/classes/Config.php
@@ -3,81 +3,82 @@ require_once('Net/SSH2.php');
 
 class Config
 {
-    public static $config;
-    public static $mailconfig;
+	public static $config;
+	public static $mailconfig;
+	public static $basepath;
 
-    public static function load()
-    {
-        if (self::$config == null) {
-            $configFile = dirname(__FILE__) . '/../config.json';
-            if (!file_exists($configFile)){
-                Response::$error = 'no_config_found';
-                Response::send();
-            }
-            self::$config = json_decode(file_get_contents($configFile));
-            self::$mailconfig = json_decode(self::remote_load());
-        }
-    }
+	public static function load()
+	{
+		if (self::$config == null) {
+			$configFile = dirname(__FILE__) . '/../config.json';
+			if (!file_exists($configFile)) {
+				Response::$error = 'no_config_found';
+				Response::send();
+			}
+			self::$config = json_decode(file_get_contents($configFile));
+			self::$mailconfig = json_decode(self::remote_load());
+		}
+	}
 
-    private static function remote_load()
-    {
-        $output = self::remote_command('getConfig ' . self::$config->mailconfig);
-		if ($output[0] !== '{'){
+	private static function remote_load()
+	{
+		$output = self::remote_command('getConfig ' . self::$config->mailconfig);
+		if ($output[0] !== '{') {
 			Response::$error = 'remote_error';
 			Response::$data = $output;
 		}
-        return $output;
-    }
+		return $output;
+	}
 
-    private static function remote_command($command)
-    {
-        $ssh = new NET_SSH2(self::$config->ssh_host);
+	private static function remote_command($command)
+	{
+		$ssh = new NET_SSH2(self::$config->ssh_host);
 
-        if (!$ssh->login(self::$config->ssh_user, self::$config->ssh_pass)) {
-            Response::$error = 'remote_login_failed';
-            Response::send();
-        }
+		if (!$ssh->login(self::$config->ssh_user, self::$config->ssh_pass)) {
+			Response::$error = 'remote_login_failed';
+			Response::send();
+		}
 
-        $output = $ssh->exec('sudo ' . Config::$config->api_command . ' ' . $command);
+		$output = $ssh->exec('sudo ' . Config::$basepath . '/ssh/api_command.sh' . ' ' . $command);
 
-        unset($ssh, $command);
-        return $output;
-    }
+		unset($ssh, $command);
+		return $output;
+	}
 
-    public function save()
-    {
-        self::remote_save();
-        self::reload();
-    }
+	public function save()
+	{
+		self::remote_save();
+		self::reload();
+	}
 
-    private static function remote_save()
-    {
-        $configString = self::getConfigJson();
-        $output = self::remote_command("setConfig '$configString' " . self::$config->mailconfig);
-        if ($output != '') {
-            Response::$error = 'remote_error';
-            Response::$data = $output;
-            Response::send();
-        }
-    }
+	private static function remote_save()
+	{
+		$configString = self::getConfigJson();
+		$output = self::remote_command("setConfig '$configString' " . self::$config->mailconfig);
+		if ($output != '') {
+			Response::$error = 'remote_error';
+			Response::$data = $output;
+			Response::send();
+		}
+	}
 
-    private static function getConfigJson()
-    {
-        return JsonFormatter::format(json_encode(self::$mailconfig, JSON_UNESCAPED_SLASHES));
-    }
+	private static function getConfigJson()
+	{
+		return JsonFormatter::format(json_encode(self::$mailconfig, JSON_UNESCAPED_SLASHES));
+	}
 
-    public function reload()
-    {
-        self::remote_reload();
-    }
+	public function reload()
+	{
+		self::remote_reload();
+	}
 
-    private static function remote_reload()
-    {
-        $output = self::remote_command('reloadConfig');
-        if (strpos($output, '200 OK') === -1) {
-            Response::$error = 'remote_error';
-            Response::$data = $output;
-            Response::send();
-        }
-    }
+	private static function remote_reload()
+	{
+		$output = self::remote_command('reloadConfig');
+		if (strpos($output, '200 OK') === -1) {
+			Response::$error = 'remote_error';
+			Response::$data = $output;
+			Response::send();
+		}
+	}
 }
--- web/config.php
+++ web/config.php
@@ -5,13 +5,14 @@ set_include_path(dirname(__FILE__) . '/classes');
 
 function __autoload($class_name)
 {
-    $file = dirname(__FILE__) . '/classes/' . $class_name . '.php';
+	$file = dirname(__FILE__) . '/classes/' . $class_name . '.php';
 
-    if (file_exists($file)) {
-        require_once $file;
-    }
+	if (file_exists($file)) {
+		require_once $file;
+	}
 }
 
+Config::$basepath = dirname(dirname(__FILE__));
 Config::load();
 
 $auth = isset($_REQUEST['auth']) ? $_REQUEST['auth'] : null;
