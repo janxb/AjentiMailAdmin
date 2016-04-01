9c188d140dbf3ecd10ba319793b47bb87a2d970d
janxb <github@janbrodda.de>
2016-04-01 15:43:58 +0200

    Moved Translations to external javascript file

--- web/index.js
+++ web/index.js
@@ -135,70 +135,7 @@ var App = function () {
     }
 };
 
-var english = {
-    login: "Login",
-    loginfailed: "Can't log in. Maybe you entered the wrong login data?",
-    forwarders: "Forwarding adresses",
-    forwardersdisabled: "Mail forwarding is not enabled for your account. Please contact your mail administrator.",
-    changepassword: "Change mailbox password",
-    delete: "Delete",
-    logintext: "Please enter your email account login details.",
-    email: "Email address",
-    password: "Password",
-    newpass1: "New password",
-    newpass2: "Repeat new password",
-    save: "Save",
-    passwordsmustmatch: "The entered passwords are not the same. Please try again..",
-    passwordischanged: "Your password has been changed.",
-    passwordchangefailed: "Can't change your password. Maybe try again later?",
-    emailnotvalid: "The entered email address is not valid!",
-    logout: "Logout",
-    headline:"Email Account Management for adress "
-}
-
-var german = {
-    login: "Login",
-    loginfailed: "Anmeldung fehlgeschlagen. Sind die Anmeldedaten korrekt?",
-    forwarders: "Email Weiterleitungen",
-    forwardersdisabled: "Email Weiterleitungen sind für den Account nicht aktiviert. Bitte an den Email Administrator wenden.",
-    changepassword: "Account-Passwort ändern",
-    delete: "Löschen",
-    logintext: "Account Login-Daten eingeben.",
-    email: "Email-Adresse",
-    password: "Passwort",
-    newpass1: "Neues Passwort",
-    newpass2: "Passwort wiederholen",
-    save: "Speichern",
-    passwordsmustmatch: "Die beiden Passwörter sind nicht identisch. Bitte erneut versuchen..",
-    passwordischanged: "Das Passwort wurde geändert.",
-    passwordchangefailed: "Passwort konnte nicht geändert werden. Bitte später erneut versuchen..",
-    emailnotvalid: "Die eingegebene Email-Adresse existiert nicht.",
-    logout: "Abmelden",
-    headline:"Email Account-Verwaltung von "
-}
-
-var greek = {
-    login: "Σύνδεση",
-    loginfailed: "Αδυναμία σύνδεσης. Μήπως έχετε δώσει λάθος στοιχεία;",
-    forwarders: "Διευθύνσεις Προώθησης",
-    forwardersdisabled: "Η προώθηση των email δεν είναι ενεργοποιημένη. Επικοινωνήστε με τον διαχειριστή.",
-    changepassword: "Αλλαγή κωδικού",
-    delete: "Διαγραφή",
-    logintext: "Πληκτρολογήστε τα στοιχεία σύνδεσης του email σας",
-    email: "Διεύθυνση Email",
-    password: "Κωδικός",
-    newpass1: "Νέος Κωδικός",
-    newpass2: "Επανάληψη Νέου Κωδικού",
-    save: "Αποθήκευση",
-    passwordsmustmatch: "Οι κωδικοί δεν είναι ίδιοι. Προσπαθήστε ξανά...",
-    passwordischanged: "Ο κωδικός σας έχει τροποποιηθεί.",
-    passwordchangefailed: "Αδυναμία αλλαγής του κωδικού. Θέλετε να δοκιμάστε αργότερα;",
-    emailnotvalid: "Δεν είναι σωστός ο κωδικός που πληκτρολογήσατε!",
-    logout: "Αποσύνδεση",
-    headline:"Διαχείριση Email για την διεύθυνση "
-}
-
 var app = new App();
 app._init();
-app.lang(german);
+app.lang(Translations.german);
 ko.applyBindings(app);
